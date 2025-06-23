727bceee886df1a30fb182d9023067d27e1c8955
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-06-23 15:31:26 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "@stardazed/streams-polyfill";
+import { Toaster } from "sonner";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster/>
       </body>
     </html>
   );
